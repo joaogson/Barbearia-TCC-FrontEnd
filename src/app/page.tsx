88617963a15d616bb18481fa139c3d249fb8d8e8
João Gabriel M. Sonalio <126979581+joaogson@users.noreply.github.com>
2025-11-11 13:18:28 -0300

    teste hospedagem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="home" style={{ color: "#3E301B", fontSize: "1.6rem" }}>
       {" "}
+      <h2>TESTE</h2>
       Pagina Inicial{" "}
     </div>
   );
