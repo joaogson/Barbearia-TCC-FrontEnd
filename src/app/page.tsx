44ce757a0c58ce13ca7c32a08af4cf453bb5e480
João Gabriel M. Sonalio <126979581+joaogson@users.noreply.github.com>
2025-11-11 16:16:36 -0300

    update inicio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div className="home" style={{ color: "#3E301B", fontSize: "1.6rem" }}>
       {" "}
-      <h2>TESTE</h2>
-      Pagina Inicial{" "}
+      <h2 style={{ borderBottom: "3px solid #3e301b", width: "70%", textAlign: "start", color: "#3e301b", fontSize: "2rem", marginTop: "25px" }}>INICIO</h2>
+      
     </div>
   );
 }
