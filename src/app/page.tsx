222545b99a3569413aab52583f60b8ac5b3fd5de
joaogson <126979581+joaogson@users.noreply.github.com>
2025-11-11 18:20:26 -0300

    teste CORS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 "use client";
 
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    console.log("VERCEL ESTÁ USANDO ESTA URL DE API:", process.env.NEXT_PUBLIC_API_URL);
+  }, []);
+
   return (
     <div className="home" style={{ color: "#3E301B", fontSize: "1.6rem" }}>
       {" "}
-      <h2 style={{ borderBottom: "3px solid #3e301b", width: "70%", textAlign: "start", color: "#3e301b", fontSize: "2rem", marginTop: "25px" }}>INICIO</h2>
-      
+      <h2 style={{ borderBottom: "3px solid #3e301b", width: "70%", textAlign: "start", color: "#3e301b", fontSize: "2rem", marginTop: "25px" }}>
+        INICIO
+      </h2>
     </div>
   );
 }
