4a6345f799cf811dea27461821de64b81b5d7389
joaogson <126979581+joaogson@users.noreply.github.com>
2025-11-12 00:22:19 -0300

    uodate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="home" style={{ color: "#3E301B", fontSize: "1.6rem" }}>
       {" "}
       <h2 style={{ borderBottom: "3px solid #3e301b", width: "70%", textAlign: "start", color: "#3e301b", fontSize: "2rem", marginTop: "25px" }}>
-        INICIO
+        Inicio
       </h2>
     </div>
   );
