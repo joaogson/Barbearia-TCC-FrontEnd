f412f2fa4dda58ac4f190f3d350d102d8756c08e
joaogson <126979581+joaogson@users.noreply.github.com>
2025-11-12 01:07:15 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@
 import Image from "next/image";
 import ImagemBarbeariaLogo from "../../public/barbearia.png";
 import ListarServicos from "components/ManagerServices/ListServicosSelection/ListarServicos";
-
+import "./inicio.css";
 export default function Home() {
   return (
     <div className="home" style={{ color: "#3E301B", fontSize: "1.6rem" }}>
       {" "}
       <h2 style={{ borderBottom: "3px solid #3e301b", width: "70%", textAlign: "start", color: "#3e301b", fontSize: "2rem", marginTop: "25px" }}>
-        <Image className="Image" style={{ width: "95%", textAlign: "center" }} src={ImagemBarbeariaLogo} alt="Header logo"></Image>
+        <Image className="image" src={ImagemBarbeariaLogo} alt="Header logo"></Image>
       </h2>
     </div>
   );
