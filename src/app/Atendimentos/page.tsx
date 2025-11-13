7ff33df6539e2a931ba61ae175b5b5a589ecb58f
João Gabriel M. Sonalio <126979581+joaogson@users.noreply.github.com>
2025-11-13 16:05:23 -0300

    update

--- src/app/Atendimentos/page.tsx
+++ src/app/Atendimentos/page.tsx
@@ -3,7 +3,16 @@ import ListServices from "../../../components/ListServices/listCostumerServices"
 export default function Atendimentos() {
   return (
     <div>
-      <h1 style={{ borderBottom: "3px solid #3e301b", width: "70%", textAlign: "start", color: "#3e301b", fontSize: "2rem", marginTop: "25px" }}>
+      <h1 
+        style={{
+          borderBottom: "3px solid #3e301b",
+          width: "80%",
+          textAlign: "start",
+          color: "#3e301b",
+          fontSize: "2rem",
+          marginTop: "25px",
+        }}
+      >
         Atendimentos
       </h1>
       <ListServices />
--- src/app/Barbers/page.tsx
+++ src/app/Barbers/page.tsx
@@ -1,9 +1,19 @@
 import BarberList from "../../../components/ListarBarbers/BarberList/BarberList";
+import "./barber.css"
 
 export default function BarbersPage() {
   return (
-    <div>
-      <h1 style={{ borderBottom: "3px solid #3e301b", width: "70%", textAlign: "start", color: "#3e301b", fontSize: "2rem", marginTop: "25px" }}>
+    <div className="page-config-container">
+      <h1 
+        style={{
+          borderBottom: "3px solid #3e301b",
+          width: "80%",
+          textAlign: "start",
+          color: "#3e301b",
+          fontSize: "2rem",
+          marginTop: "25px",
+        }}
+      >
         Barbeiros
       </h1>
       <BarberList />
